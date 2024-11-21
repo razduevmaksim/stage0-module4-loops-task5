4410f9e81b6f55f3ad76c1d04540643fe0f74b9b
mrazduev <mrazduev@gmail.com>
2024-11-21 15:50:12 +0300

    Module 4.2. Task 5. Nested Loops

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,37 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
+        for (int i = 0; i < height / 2; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < height - 2 * i; j++) {
+                System.out.print("8");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
 
+        int middleOfFigure;
+
+        if (height % 2 == 0)
+            middleOfFigure = height / 2 - 1;
+        else
+            middleOfFigure = height / 2;
+
+        for (int i = middleOfFigure; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < height - 2 * i; j++) {
+                System.out.print("8");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task5;
 
 public class Square {
     public void printSquareFrom8s(int sideLength){
-
+        for (int i = 1; i <= sideLength; i++) {
+            for (int j = 1; j <= sideLength; j++) {
+                if (j == 1 || j == sideLength || i == 1 || i == sideLength)
+                    System.out.print("8");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
